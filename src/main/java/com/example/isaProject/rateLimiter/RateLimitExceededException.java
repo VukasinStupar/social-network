4bd848f41d487c470e7bef7c4e2ae43987741903
VukasinStupar <stupar93vukasin@gmail.com>
2025-07-06 13:43:39 +0200

    rate limiter fix

--- src/main/java/com/example/isaProject/rateLimiter/RateLimitExceededException.java
+++ src/main/java/com/example/isaProject/rateLimiter/RateLimitExceededException.java
@@ -2,6 +2,7 @@ package com.example.isaProject.rateLimiter;
 
 public class RateLimitExceededException extends RuntimeException {
     public RateLimitExceededException(String message) {
+
         super(message);
     }
 }
